355722ebeb6d27e38715fd9953d8ed404558c435
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 01:42:33 +0300

    problem5
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem5
+{
+    internal class Program
+    {
+        public static bool Checker(int val)
+        {
+            var flag = true;
+            for (var i = 1; i < 20; i++)
+                if (val % i != 0)
+                {
+                    flag = false;
+                    break;
+                }
+
+            return flag;
+        }
+
+        public static void Main(string[] args)
+        {
+            var i = 1;
+            while (true)
+                if (Checker(i))
+                {
+                    Console.WriteLine(i.ToString());
+                    break;
+                }
+        }
+    }
+}
