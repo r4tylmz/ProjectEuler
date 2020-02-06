3abc542a67460d4757b4f22289f7fa680fc427c9
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 00:47:34 +0300

    problem2
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int first = 1, second = 1, temp = 0, sum = 0;
+            while (temp < 4000000)
+            {
+                if (temp % 2 == 0)
+                    sum += temp;
+
+                temp = first + second;
+                first = second;
+                second = temp;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
