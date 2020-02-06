95e8e4daf1fa32dd768ac6817abf89902a672e86
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 02:11:53 +0300

    problem7
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problem7
+{
+    internal class Program
+    {
+        private static bool IsPrime(int number)
+        {
+            var control = true;
+            for (var i = 2; i < number; i++)
+                if (number % i == 0)
+                {
+                    control = false;
+                    break;
+                }
+
+            return control;
+        }
+
+        public static void Main(string[] args)
+        {
+            int count = 1, i = 2;
+            while (count <= 10001)
+            {
+                if (IsPrime(i)) count++;
+                i++;
+            }
+
+            Console.WriteLine(i - 1);
+        }
+    }
+}
