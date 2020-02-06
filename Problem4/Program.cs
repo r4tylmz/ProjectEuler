dc4ac48899d1fdb199da5ab24fcfb835a6512340
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 01:31:27 +0300

    problem4
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem4
+{
+    internal class Program
+    {
+        private static bool CheckPalindrome(string str)
+        {
+            var arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return str.Equals(new string(arr));
+        }
+
+        public static void Main(string[] args)
+        {
+            var list = new List<int>();
+            for (int i = 1000 - 1; i >= 100; i--)
+            {
+                for (int j = 1000 - 1; j >= 100; j--)
+                {
+                    var mul = i * j;
+                    if (CheckPalindrome(mul.ToString()))
+                    {
+                        list.Add(mul);
+                    }
+                }
+            }
+
+            Console.WriteLine(list.Max().ToString());
+        }
+    }
+}
