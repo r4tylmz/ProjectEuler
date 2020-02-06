f3d55062738defa1fedf17e32a5c8835aa5f3841
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 00:16:55 +0300

    problem1
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 | i % 5 == 0)
+                    sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
