2376ac030580be6db454f070cabeeba80feee28f
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 01:00:40 +0300

    problem3
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var number = 600851475143;
+            long i;
+            for (i = 2; i < number; i++)
+                if (number % i == 0)
+                    number /= i;
+            Console.WriteLine(i);
+        }
+    }
+}
