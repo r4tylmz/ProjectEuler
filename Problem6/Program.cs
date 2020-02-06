cd043eede2ba5212b7445e5121e5be9d88a6208a
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 01:48:38 +0300

    problem6
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problem6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sumofsquares = (100 * 101 * 201) / 6;
+            var squareofsum = Math.Pow(100 * 101 / 2, 2);
+            Console.WriteLine(squareofsum - sumofsquares);
+        }
+    }
+}
