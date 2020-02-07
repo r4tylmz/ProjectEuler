bf1f4e954bf6b09a4082641dd94f4caa2f59f01b
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 20:52:13 +0300

    problem9
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problem9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int c;
+            for (int a = 3; a < 333; a++)
+            {
+                for (int b = a + 1; b < 500; b++)
+                {
+                    c = 1000 - a - b;
+                    if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                        Console.WriteLine(a * b * c);
+                }
+            }
+        }
+    }
+}
