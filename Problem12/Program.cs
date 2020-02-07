0f7a9f40b7d2c7dffc50f20ea5903c2d9f5f61d3
r4tylmz <47759816+r4tylmz@users.noreply.github.com>
2020-02-07 23:26:12 +0300

    problem12
    Signed-off-by: r4tylmz <47759816+r4tylmz@users.noreply.github.com>

---
+++ Problem12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Problem12
+{
+    internal class Program
+    {
+        private static int GetDivisors(int number)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    if (i * i < number)
+                        count += 2;
+                    else
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            int i = 2, number = 1;
+            while (GetDivisors(number) < 500)
+            {
+                number += i;
+                i++;
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
